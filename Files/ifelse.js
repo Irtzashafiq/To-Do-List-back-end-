845084003b4ee94887f459a3f690aa324fded2af
Irtza Shafiq <irtzashafiq0@gmail.com>
2024-05-13 00:46:23 +0500

    sunday

---
+++ Files/ifelse.js
@@ -0,0 +1,43 @@
+// If else conditions
+
+// let hour = 19
+
+// if (hour <= 11) {
+//     console.log("Good Morning")
+
+// } else if(hour > 11 && hour < 18) {
+//     console.log(
+//         "Good Noon"
+//     )
+// }  else {
+//     console.log("Good Night")
+// }
+
+// Switch statement 
+
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = "sunday"
+//         break;
+//         case 1:
+//             day = "monday"
+//             break;
+//             case 2:
+//                 day = "tuesday"
+//                 break;
+//                 case 3:
+//                     day = "wednesday"
+//                     break;
+//                     case 4:
+//                         day = "thursday"
+//                         break;
+//                         case 5:
+//                             day = "friday"
+//                             break;
+//                             case 6:
+//                                 day = "saturday"
+//                                 break;
+        
+// }
+// console.log(day)
+
