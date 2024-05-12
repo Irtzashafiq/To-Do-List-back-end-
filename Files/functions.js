b529a4e44e8e785edec04b3faff682c337463452
Irtza Shafiq <irtzashafiq0@gmail.com>
2024-05-13 00:20:41 +0500

    sunday

---
+++ Files/functions.js
@@ -0,0 +1,53 @@
+//Fuctions
+//A function starts with keyword function.. A simple syntax of a function 
+//A function had a function name and some parameters and a return value and end the function after returning the value
+// function functions(p1,p2) {
+//     return p1*p2;
+// }
+// console.log(functions(2,5))
+
+
+// let x = myFunction(4,8);
+
+// function myFunction(a,b) {
+//     return a + b
+// }
+
+// console.log(x)
+
+// fahrenheit to celcius function
+
+// function toCelcius(fahrenheit) {
+//     return (5/9) * (fahrenheit - 32)
+// }
+
+// let text = "The temprature is " + toCelcius(77)+ " Celcius";
+// console.log(text)
+
+// local variables inside a function
+
+// function myFunction(){
+//     let carName = "Lexus"
+//     console.log(carName) 
+//     //code here can use carName
+// }
+// console.log(carName)//code outside the function cannot use canName
+
+//Arrow Functions
+// These are updated form of functions it shortens the code
+
+//simple function
+
+// hello = function () {
+//     return "Hello World"
+    
+// }
+
+// //Arrow Function
+// hello =  () => {
+//     return "Hello function"
+// }
+
+// // more shorten way if we have only one statement
+// hello = () => "Hello Arrow function"
+// newf = () => "Hello newf function"
